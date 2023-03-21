3494e8579e7700258e981d5c6105a31906bd6fb0
Ruben <rubenmontes330@gmail.com>
2023-03-21 21:07:58 +0000

    Páginas, Link y code splitting

---
+++ pages/chanchito.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+const chanchito = () => {
+  return (
+    <div>
+      <div>chanchito triste</div>
+        <Link href={"/"}>inicio</Link>
+    </div>
+    
+  )
+}
+
+export default chanchito
