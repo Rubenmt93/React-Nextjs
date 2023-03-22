76d07b2b26a33c104aa154e244daa2454e1f3ffe
Ruben <rubenmontes330@gmail.com>
2023-03-22 18:12:37 +0000

    Rutas dinamicas anidadas

---
+++ pages/pokemones/[id]/descripcion.js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router"
+
+const Desc = () => {
+    const router = useRouter
+
+    return (
+        <div>
+            Descripcion de Pokemons
+        </div>
+    )
+}
+
+export default Desc
