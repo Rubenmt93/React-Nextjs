f2c3a10c4e15af8d91c855a3e442852b79949a76
Ruben <rubenmontes330@gmail.com>
2023-03-22 18:22:55 +0000

    Componente Image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import Coffee from '../public/cofee.jpg'
 
 
 export default function Home() {
@@ -6,6 +8,10 @@ export default function Home() {
     <div>
         <Link href={'/chanchitos/feliz'}>Ir a chanchitos</Link>
         <p>Hola cerdo</p>
+
+        {/* <Image src = '/cofee.jpg' width={400} height= {400}/> */}
+        {/* <Image src = '/cofee.jpg' layout='fill'/> */}
+        <Image src = {Coffee} width={400} height= {400}/>
 
     </div>
   )
