f3295efdf9d219b13afe8254388cad11ce5ade84
Ruben <rubenmontes330@gmail.com>
2023-03-23 14:47:10 +0000

    Configuracion de Testunit

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import Coffee from '../public/cofee.jpg'
 
 
 export default function Home() {
   return (
-    <div>
-        <Link href={'/chanchitos/feliz'}>Ir a chanchitos</Link>
-        <p>Hola cerdo</p>
-
-        {/* <Image src = '/cofee.jpg' width={400} height= {400}/> */}
-        {/* <Image src = '/cofee.jpg' layout='fill'/> */}
-        <Image src = {Coffee} width={400} height= {400}/>
-
+    <div>      
+      <p>Pokemones</p>
     </div>
   )
 }
